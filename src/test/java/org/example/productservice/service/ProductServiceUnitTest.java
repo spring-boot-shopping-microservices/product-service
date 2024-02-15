c253e300e8265c3e97492c137b494c24ff7f5b39
Aamir shaikh <aamirshaikh3232@outlook.com>
2024-02-15 22:44:53 +0530

    Rename ProductServiceTest to ProductServiceUnitTest

--- src/test/java/org/example/productservice/service/ProductServiceUnitTest.java
+++ src/test/java/org/example/productservice/service/ProductServiceUnitTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-class ProductServiceTest {
+class ProductServiceUnitTest {
   @Mock
   private ProductRepository productRepository;
 
